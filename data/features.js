961b6908ec310456dce7a93b0df11b895214f2a4
GiedriusTavaras <tavaraz@gmail.com>
2020-07-30 15:37:52 +0300

    Features #12

---
+++ data/features.js
@@ -0,0 +1,22 @@
+const features = [
+    {
+        size: 'image-capture feature col-12 col-md-6 col-lg-4 col-xl-4',
+        icon: 'fa fa-camera',
+        title: 'Image Capture',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate aperiam placeat quae perspiciatis reprehenderit.'
+    },
+    {
+        size: 'customization feature col-12 col-md-6 col-lg-4 col-xl-4',
+        icon: 'fa fa-pencil-square-o',
+        title: 'Customization',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate aperiam placeat quae perspiciatis reprehenderit.'
+    },
+    {
+        size: 'data-security feature col-12 col-lg-4 col-xl-4',
+        icon: 'fa fa-lock',
+        title: 'Data security',
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt earum ullam corrupti odit recusandae qui!'
+    }
+]
+
+export default features;
