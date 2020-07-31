2de1740ab9e6611da49afa2cd5f9b316838e47b9
GiedriusTavaras <tavaraz@gmail.com>
2020-07-31 08:32:50 +0300

    vertical-dash

--- data/features.js
+++ data/features.js
@@ -1,18 +1,18 @@
 const features = [
     {
-        size: 'image-capture feature col-12 col-md-6 col-lg-4 col-xl-4',
+        size: 'image-capture feature  ',
         icon: 'fa fa-camera',
         title: 'Image Capture',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate aperiam placeat quae perspiciatis reprehenderit.'
     },
     {
-        size: 'customization feature col-12 col-md-6 col-lg-4 col-xl-4',
+        size: 'customization feature  ',
         icon: 'fa fa-pencil-square-o',
         title: 'Customization',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam voluptate aperiam placeat quae perspiciatis reprehenderit.'
     },
     {
-        size: 'data-security feature col-12 col-lg-4 col-xl-4',
+        size: 'data-security feature  ',
         icon: 'fa fa-lock',
         title: 'Data security',
         description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt earum ullam corrupti odit recusandae qui!'
