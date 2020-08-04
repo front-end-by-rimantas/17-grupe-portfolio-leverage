8a43462db83fe9a64ad436b640e76caab5e17419
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-08-04 16:05:30 +0300

    Initial commit

---
+++ data/testimonials.js
@@ -0,0 +1,29 @@
+const testimonials = [
+    {
+        name: 'adams baker',
+        content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, nemo!',
+        rating: 5
+    },
+    {
+        name: 'mary evans',
+        content: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit.',
+        rating: 4
+    },
+    {
+        name: 'sarah lopez',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quis ad quibusdam?',
+        rating: 5
+    },
+    {
+        name: 'joseph hills',
+        content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, ipsam delectus.',
+        rating: 4
+    },
+    {
+        name: 'karen usman',
+        content: 'Lorem ipsum dolor sit amet.',
+        rating: 3
+    }
+]
+
+export {testimonials};
---
+++ js/renderTestimonials.js
@@ -0,0 +1,5 @@
+function renderTestimonials(data, selector) {
+    return;
+}
+
+export {renderTestimonials};
