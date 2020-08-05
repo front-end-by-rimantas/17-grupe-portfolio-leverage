4eeebb4fbb2797e7926f1049186417f8c97a8d57
Edgaras <baroniunas@gmail.com>
2020-08-05 17:03:16 +0300

    Update renderLightBox.js logic

--- js/renderLightBox.js
+++ js/renderLightBox.js
@@ -1,10 +1,10 @@
 function renderLightBox(image, count, size) {
     const DOM = document.querySelector('.light-box .image div');
-    const arraySize = document.querySelector('.light-box .gallery');
-    const HTML = image;
-    const domImage = DOM.innerHTML = HTML;
+    const galleryDOM = document.querySelector('.light-box .gallery');
+
+    DOM.innerHTML = image;
     const text = count + 1 + "/" + size;
-    const domSize = arraySize.innerText = text;
+    galleryDOM.innerText = text;
     return;
 }
 export default renderLightBox;
