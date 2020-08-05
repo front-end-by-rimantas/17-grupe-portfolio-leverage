4e5b5676ef5920654199305e9dcef0d10bcf3db0
Edgaras <baroniunas@gmail.com>
2020-08-05 10:29:08 +0300

    Second fix arraySize name

--- js/renderLightBox.js
+++ js/renderLightBox.js
@@ -1,6 +1,6 @@
 function renderLightBox(image, count, size) {
     const DOM = document.querySelector('.light-box .image div');
-    const ArraySize = document.querySelector('.light-box .gallery');
+    const arraySize = document.querySelector('.light-box .gallery');
     const HTML = image;
     const domImage = DOM.innerHTML = HTML;
     const text = count + 1 + "/" + size;
