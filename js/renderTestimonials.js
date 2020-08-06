6ad70f4449e6da130276369ad1bfae8b2d7a5ede
LaurynasAnd <67257508+LaurynasAnd@users.noreply.github.com>
2020-08-06 17:07:17 +0300

    working cards scroll on buttons press

--- js/renderTestimonials.js
+++ js/renderTestimonials.js
@@ -1,14 +1,4 @@
 
-/* const widthHolder = document.querySelector('#testimonials .testimonials');
-    const holderWidth = widthHolder.offsetWidth;
-    const winWidth = window.innerWidth;
-    let width = 0;
-    if (winWidth < 768) width = holderWidth;
-    else if (winWidth < 1023) width = holderWidth/2;
-    else if (winWidth < 1200) width = (Math.floor(holderWidth/3));
-    else width = holderWidth/3;
-    return width;*/
-
 function renderTestimonials(data, selector) {
     renderTestimonialsStructure(data, selector);
     renderCards(data);
@@ -16,9 +6,14 @@ function renderTestimonials(data, selector) {
 
 function renderTestimonialsStructure(data, selector) {
     const DOM = document.querySelector(selector);
+    const winWidth = window.innerWidth;
+    let cardsShown = 1;
+    if (winWidth < 768) cardsShown = 1;
+    else if (winWidth < 1023) cardsShown = 2;
+    else cardsShown = 3;
     let navbar = '';
-    for (let i = 0; i < data.length; i++){
-        navbar += `<div class="nav-line line${i+1}"></div>`;
+    for (let i = 0; i < data.length -cardsShown + 1; i++){
+        navbar += `<div class="nav-line line${i}${i===0 ? ' active' : ''}"></div>`;
     }
     let HTML = `<div class="row">
                     <div class="intro col-12">
@@ -27,23 +22,10 @@ function renderTestimonialsStructure(data, selector) {
                     </div>
                 </div>
                 <div class="testimonials">
-                    <div class="overflow-holder"></div>
+                    <div class="overflow-holder" style="transform: translate(0px);"></div>
                     <div class="navbar">${navbar}</div>
                 </div>`;
     return DOM.innerHTML = HTML;
-}
-
-function testimonialsEvents(){
-    // testimonials event listener to resize cards
-    const cards = document.querySelectorAll('#testimonials .testimonial');
-    window.addEventListener('resize', ()=> resizeCard(cards));
-
-    // testimonials event to scroll cards
-    const navbar = document.querySelectorAll('#testimonials .nav-line');
-    for (const navLine of navbar) {
-        const number = parseInt((navLine.classList)[1].match(/[0-9]/g));
-        navLine.addEventListener('click', () => scrollTestimonials(navLine))
-    }
 }
 
 //function to render cards
@@ -69,23 +51,33 @@ function renderCards(data){
     return DOM.innerHTML = HTML;
 }
 
+function testimonialsEvents(){
+    // testimonials event listener to resize cards and change navbar size
+    const cards = document.querySelectorAll('#testimonials .testimonial');
+    const bars = document.querySelectorAll('#testimonials .nav-line');
+    window.addEventListener('resize', ()=> resizeCard(cards, bars));
+
+    // testimonials event to scroll cards with buttons
+    const navbar = document.querySelectorAll('#testimonials .nav-line');
+    for (const navLine of navbar) {
+        const number = parseInt((navLine.classList)[1].match(/[0-9]/g));
+        navLine.addEventListener('click', () => scrollTestimonials(navLine, number))
+    }
+}
+
+
 //function to resize card when changing screen size
-function resizeCard(cards) {
+function resizeCard(cards, bars) {
     const width = getCardWidth();
     for(const card of cards) {
         card.style.width = `${width}px`;
     }
+
+    return;
 }
 
 //function to get width
 function getCardWidth(){
-    // const winWidth = window.innerWidth;
-    // let width = 0;
-    // if (winWidth < 768) width = winWidth-67;
-    // else if (winWidth < 1023) width = (winWidth-97)/2;
-    // else if (winWidth < 1200) width = (Math.floor((winWidth-110)/3));
-    // else width = 1140/3;
-    // return width;
     const widthHolder = document.querySelector('#testimonials .testimonials');
     const holderWidth = widthHolder.offsetWidth;
     const winWidth = window.innerWidth;
@@ -98,8 +90,18 @@ function getCardWidth(){
 
 
 // function for event listener 'click'
-function scrollTestimonials(DOM) {
+function scrollTestimonials(DOM, number) {
+    const beforeDOM = DOM.closest('.navbar').querySelector('.active');
+    const beforeNumber = parseInt((beforeDOM.classList)[1].match(/[0-9]/g));
+    const difference = beforeNumber - number;
+    const cardWidth = getCardWidth();
+    const overflowHolder = document.querySelector('#testimonials .overflow-holder')
+    if (number === beforeNumber) return;
+    console.log('before',beforeDOM, beforeNumber,'after', DOM, number);
     console.log('click');
+    beforeDOM.classList.remove('active');
+    overflowHolder.style.transform = `translate(-${cardWidth*number}px)`;
+    DOM.classList.add('active');
 }
 
 export {renderTestimonials, testimonialsEvents};
